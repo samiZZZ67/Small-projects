751a5988f8dcc1cfa5ce907fd3f1a8543e05c89f
samizzz67 <abiyafikre@gmail.com>
2026-03-06 11:56:09 +0300

    Add script to fetch and print top Python repositories

--- python_repos.py
+++ python_repos.py
@@ -2,7 +2,7 @@ import requests
 import sys
 import io
 
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
 #Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept': 'application/vnd.github.v3+json'} 
@@ -19,22 +19,13 @@ print(f"Total repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
 print(f"Repositories returned: {len(repo_dicts)}")
 # Examine the first repository.
-#repo_dict=repo_dicts[0]
-#print(repo_dict['name'])
-#print(f"\nKeys: {len(repo_dict)}")
-'''for t, key in enumerate(sorted (repo_dict.keys())):
-    print(t,key)'''
+repo_dict=repo_dicts[0]
+print(repo_dict['name'])
+print(f"\nKeys: {len(repo_dict)}")
+
 print("\nSelected information about first repository:")
 
-'''for repo_dict in repo_dicts:
-    print("\nRepository Information:")
-    print(f"Name: {repo_dict.get('name')}")
-    print(f"Owner: {repo_dict.get('owner', {}).get('login')}")
-    print(f"Stars: {repo_dict.get('stargazers_count')}")
-    print(f"Repository: {repo_dict.get('html_url')}")
-    print(f"Created: {repo_dict.get('created_at')}")
-    print(f"Updated: {repo_dict.get('updated_at')}")
-    print(f"Description: {repo_dict.get('description')}")'''
+
 for repo_dict in repo_dicts:
    print(f"Name: {repo_dict['name']}")
    print(f"Owner: {repo_dict['owner']['login']}")
